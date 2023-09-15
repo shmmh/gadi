91d0363a53eb5968d323a08ab4ee6b72cc6345c0
Shammah Rashad <shammah.rashad@gmail.com>
2023-09-15 20:20:08 +0100

    setup launcer icons with icons_launcher for macos ios android

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        home: Center(
-      child: Text("Hello world!"),
-    ));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(useMaterial3: false),
+      home: const Scaffold(
+        body: SafeArea(
+          child: Center(child: Text("Hello world!")),
+        ),
+      ),
+    );
   }
 }
